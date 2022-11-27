ca529a0b3197c453f30815150e7ea31ab37b986b
Prince Narteh <prinarteh@gmail.com>
2022-11-27 06:13:45 +0000

    add divider section

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
 
       <main>
         <Header />
+        <div className="flex justify-center py-5 divide-x-2 divide-gray-500">
+          <h3 className="text-3xl px-10">Top Deals</h3>
+          <h3 className="text-3xl px-10">Product Videos</h3>
+          <h3 className="text-3xl px-10">Trending</h3>
+        </div>
       </main>
     </div>
   );
