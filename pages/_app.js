e98beabf37af561290299e9b622a1b21ac2e84bf
Prince Narteh <prinarteh@gmail.com>
2022-11-27 05:43:32 +0000

    add SubNavbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import MainNavbar from '../components/MainNavbar';
+import MainNavbar from '../components/Navbars/MainNavbar';
+import SubNavbar from '../components/Navbars/SubNavbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <MainNavbar />
+      <SubNavbar />
       <Component {...pageProps} />
     </div>
   );
