2440b2fdc8cfc68bb18542fc11a873240c380442
Prince Narteh <prinarteh@gmail.com>
2022-11-27 05:07:50 +0000

    add MainNavbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import MainNavbar from '../components/MainNavbar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <MainNavbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
